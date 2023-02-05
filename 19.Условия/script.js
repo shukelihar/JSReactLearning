5293787d0de70c748786cd637f38f45e1c17ffa4
Ihar <shukelihar@gmail.com>
2023-02-05 20:35:29 +0300

    add

--- 19.Условия/script.js
+++ 19.Условия/script.js
@@ -1,2 +1,19 @@
 "use strict";
 
+if(1) {
+    console.log('Ok!');
+} else {
+    console.log('Error');
+}
+
+const num = 50;
+
+if(num < 49) {
+    console.log('Error');
+} else if(num > 100) {
+    console.log('Много');
+} else {
+    console.log('Ok');
+}
+
+(num === 50) ? console.log('Ok') : console.log('Error');
---
+++ 20.(д) Логические операторы/script.js
@@ -0,0 +1,6 @@
+"use strict";
+
+let johnReport, alexReport, samReport, mariaReport = 'done';
+
+console.log(johnReport || alexReport || samReport || mariaReport);
+
